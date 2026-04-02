08c9fba97328362f05d446ad352d27ae0f7b8714
PchelkinStepan <3395509@bk.ru>
2026-04-02 10:46:45 +0300

    feat: добавлен новый прибор WPH-ZF с расходами на горячую воду

--- src/data/devices/48422-11/index.js
+++ src/data/devices/48422-11/index.js
@@ -2,7 +2,7 @@ import wphzf from './wphzf/wphzf.json';
 
 const device = {
   id: "48422-11",
-  name: "Счетчики холодной и горячей воды турбинные WPH-ZF.",
+  name: "Счетчики холодной и горячей воды турбинные W.",
   hasModels: true,
   hasTypes: false,
   hasClasses: false,
